5dc7dd71242f6e4e00ae031c97e582661ce1703d
Mohammed Naji <moha.buy@gmail.com>
2017-02-22 10:30:17 +0200

    fix bugs

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var router = require('./router.js');
+http.createServer(function(req, res) {
+    var path = req.method + ' ' + req.url;
+    try {
+        router[path](req, res);
+    } catch (err) {
+        console.log('path', path);
+        console.log('err', err);
+        res.end('Not found');
+    }
+}).listen(process.env.PORT || 8080, function() {
+    console.log('Listening on 8080');
+});
---
+++ frontend/index.js
@@ -0,0 +1,31 @@
+function r() {
+    var dataList = document.getElementById('json-datalist');
+    var input = document.getElementById('ajax');
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(response) {
+        var array = [];
+        var jsonOptions = "";
+        var option = "";
+        while (dataList.hasChildNodes()) {
+            dataList.removeChild(dataList.lastChild);
+        }
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                // Parse the JSON
+                jsonOptions = JSON.parse(request.responseText);
+                array = jsonOptions.slice(0, 5);
+                // console.log(array)
+                array.forEach(function(item) {
+                    option = document.createElement('option');
+                    option.value = item;
+                    dataList.appendChild(option);
+                });
+                input.placeholder = "e.g. datalist";
+            } else {
+                input.placeholder = "Couldn't load datalist options :(";
+            }
+        }
+    };
+    request.open('POST', 'http://localhost:8080/getdata', true);
+    request.send(input.value);
+}
