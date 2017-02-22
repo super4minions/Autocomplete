efc9bbaa88d158fbe4aa93809a39d4d6611eeb15
Mohammed Naji <moha.buy@gmail.com>
2017-02-22 20:17:44 +0200

    add exe time

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,9 @@
 function r() {
     var dataList = document.getElementById('json-datalist');
     var input = document.getElementById('ajax');
+   var t0 = performance.now();
+
+
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(response) {
         var array = [];
@@ -18,13 +21,18 @@ function r() {
                     option = document.createElement('option');
                     option.value = item;
                     dataList.appendChild(option);
+
                 });
+
+var t1 = performance.now();
+console.log("Call to doSomething took " + (t1 - t0)/1000 + " milliseconds.")
+
                 input.placeholder = "e.g. datalist";
             } else {
                 input.placeholder = "Couldn't load datalist options :(";
             }
         }
     };
-    request.open('POST', 'https://mohanaji-search.herokuapp.com/getdata', true);
+    request.open('POST', 'http://localhost:8080/getdata', true);
     request.send(input.value);
 }
